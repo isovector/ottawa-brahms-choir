7460168a22772d4dd19b322a1303268d839456f9
Sandy Maguire <sandy@sandymaguire.me>
2018-11-23 10:28:10 -0500

    changes

--- ottawa-brams-choir/app/Main.hs
+++ ottawa-brams-choir/app/Main.hs
@@ -1,14 +1,22 @@
+{-# LANGUAGE LambdaCase        #-}
 {-# language OverloadedStrings #-}
 
 module Main where
 
-import Data.Foldable (for_)
-import SitePipe
+import Data.Bool (bool)
+import Debug.Trace
 import Control.Lens hiding ((.=))
+import Control.Monad.IO.Class (liftIO)
+import Data.Aeson
 import Data.Aeson.Lens
+import Data.Foldable (for_)
 import Data.String.Conv (toS)
 import Data.Text (Text)
 import Data.Text.Lens (_Text)
+import Data.Time
+import Data.Time.Calendar.WeekDate
+import Data.Time.Parse (strptime)
+import SitePipe
 
 main :: IO ()
 main = site $ do
@@ -18,6 +26,7 @@ main = site $ do
     let layout = page ^?! prop "layout" . jsonString
     writeTemplate (getTemplate layout) $ pure $ page
       & prop "url" . jsonString %~ drop 6
+      & prop "event" . prop "time" %~ parseEventTime
 
   copyFilesWith (drop 7) [ "static/*" ]
 
@@ -27,11 +36,62 @@ getTemplate :: String -> FilePath
 getTemplate = ("templates/" ++) . (++ ".html")
 
 ------------------------------------------------------------------------------
--- | This is a prism but I couldn't figure out how to make it typecheck as
+-- | This is a 'Prism'' but I couldn't figure out how to make it typecheck as
 -- such.
 prop :: Applicative f => Text -> (Value -> f Value) -> Value -> f Value
 prop p = _Object . at p . _Just
 
 jsonString :: Prism' Value String
 jsonString = _String . _Text
+
+parseEventTime :: Value -> Value
+parseEventTime v =
+  let str = v ^?! jsonString
+      Just (lt, _) = strptime "%Y-%m-%d %H:%M" str
+   in object
+        [ "dow"  .= jsonString # getDayOfWeek lt
+        , "date" .= jsonString # getDate lt
+        , "time" .= jsonString # getTime lt
+        ]
+
+getDate :: LocalTime -> String
+getDate ld =
+  let (_, month, day) = toGregorian $ localDay ld
+      showMonth =
+        case month of
+          1  -> "Jan"
+          2  -> "Feb"
+          3  -> "Mar"
+          4  -> "Apr"
+          5  -> "May"
+          6  -> "Jun"
+          7  -> "Jul"
+          8  -> "Aug"
+          9  -> "Sep"
+          10 -> "Oct"
+          11 -> "Nov"
+          12 -> "Dec"
+   in showMonth ++ " " ++ show day
+
+getTime :: LocalTime -> String
+getTime ld =
+  let TimeOfDay hour minute _ = localTimeOfDay ld
+      isPM = hour >= 12
+   in if minute /= 0
+         then error "we can't show minutes!!"
+         else show (hour - bool 0 12 (hour >= 13)) ++ bool "am" "pm" isPM
+
+
+
+getDayOfWeek :: LocalTime -> String
+getDayOfWeek ld =
+  case toWeekDate (localDay ld) ^. _3 of
+    1 -> "Mon"
+    2 -> "Tue"
+    3 -> "Wed"
+    4 -> "Thu"
+    5 -> "Fri"
+    6 -> "Sat"
+    7 -> "Sun"
+
 
--- ottawa-brams-choir/site/pages/index.md
+++ ottawa-brams-choir/site/pages/index.md
@@ -1,5 +1,25 @@
 ---
 layout: home
-buy-tickets: https://www.eventbrite.ca/e/the-peace-of-christmas-tickets-51383576661
+video-url: https://www.youtube.com/embed/FBeR94Bqbxk?rel=0&start=18&showinfo=0
+
+event:
+  tickets-url: https://www.eventbrite.ca/e/the-peace-of-christmas-tickets-51383576661
+  description: >
+    Join us for a magical afternoon of choral music and caroling under the
+    musical direction of Christopher Askwith. Featuring works from Brahms,
+    Mendelssohn, Rachmaninoff, J.S. Bach, Gounod, Sch&uuml;tz, Handel and more.
+  time: 2018-12-02 15:00
+  image: choir2.jpg
 ---
+
+### About Us
+
+The Ottawa Brahms Choir was founded in 1980 by Dieter Kiesewalter as a local
+German community Choir. Currently directed by Christopher Askwith and
+accompanied by Svetlana Logigan, the choir continues to offer audiences a wide
+range of choral music, from classical to contemporary, while maintaining its
+roots in the German repertoire.
+
+The choir rehearses at Southminster United Church, on the corner of Bank and
+Aylmer, every Monday evenings at 7 pm.
 
